d024fac83468e898f9a7ee37f434b7a392d531a4
Xiaoze_Wang@JMP_PC <xiaoze_wang@hotmail.com>
2024-02-03 17:00:14 +0800

    微調圖片處理邏輯

--- MarkPiont.py
+++ MarkPiont.py
@@ -23,6 +23,42 @@ ml_types_dict = {'XGS': ['SOBBK', 'ICEBK', 'BGI', 'SOB', 'ICE'], 'BGS': ['SOBBK'
                  'CGS': ['SOB', 'ICE']}
 
 
+class UnitFolder(object):
+
+    def __init__(self, unit_folder_path: Path, station_name: str = 'BGS'):
+        self._station_name = station_name
+        self._ml_types = ml_types_dict.get(station_name, ml_types_dict['XGS'])
+        if unit_folder_path.is_dir():
+            self._folder_path = unit_folder_path
+        elif unit_folder_path.is_file():
+            print(
+                f'Waring: 初始化路径[{unit_folder_path}]是文件,不是目录,强制转换为文件的父目录[unit_folder_path.parent]')
+            self._folder_path = unit_folder_path.parent
+        else:
+            raise TypeError(f'Error:初始化失败,初始化路径[{unit_folder_path}]类型错误,应该为目录或者文件.')
+
+    @property
+    def unit_folder_path(self):
+        return self._folder_path
+
+    @property
+    def station_name(self):
+        return self._station_name
+
+    def parse_ml_folder(self) -> dict:
+        file_suffixes = ['txt', 'JPG']
+        ml_folder_files = {}
+        for ml_type in self._ml_types:
+            ml_files = file_name(
+                self._folder_path, fr'.*\.{ml_type}\.({"|".join(file_suffixes)})$')
+            if ml_files:
+                ml_folder_files.update(
+                    {ml_type[:3]: {_.suffix[1:]: _ for _ in ml_files}})
+            else:
+                ml_folder_files.update({ml_type[:3]: {}})
+        return ml_folder_files
+
+
 def parse_ml_folder(unit_folder: Path, ml_types: list = None, file_suffixes: list = None) -> dict:
     if ml_types is None:
         ml_types = ['SOBBK', 'ICEBK', 'BGI', 'SOB', 'ICE']
@@ -30,15 +66,15 @@ def parse_ml_folder(unit_folder: Path, ml_types: list = None, file_suffixes: lis
         file_suffixes = ['txt', 'JPG']
     if unit_folder.is_file():
         unit_folder = unit_folder.parent
-    ml_folder_files = []
+    ml_folder_files = {}
     for ml_type in ml_types:
         ml_files = file_name(
             unit_folder, fr'.*\.{ml_type}\.({"|".join(file_suffixes)})$')
         if ml_files:
-            ml_folder_files.append(
-                {ml_type: {_.suffix[1:]: _ for _ in ml_files}})
+            ml_folder_files.update(
+                {ml_type[:3]: {_.suffix[1:]: _ for _ in ml_files}})
         else:
-            ml_folder_files.append({ml_type: {}})
+            ml_folder_files.update({ml_type[:3]: {}})
     return {unit_folder.name: ml_folder_files}
 
 
@@ -59,18 +95,18 @@ def logmod(num: int):
 def mark_bgi_ml_image():
     font = cv2.FONT_HERSHEY_SIMPLEX
     from LoadFilePath import ml_station
-    ml_types = ml_types_dict.get(ml_station,ml_types_dict['XGS'])
-    for kk, unit_folder_path in enumerate(data_pardir.iterdir()):
-        print(f"--开始检查第 {kk + 1} 個機臺{unit_folder_path.name} ML 信息")
+    ml_types = ml_types_dict.get(ml_station, ml_types_dict['XGS'])
+    for kkk, unit_folder_path in enumerate(data_pardir.iterdir()):
+        print(f"--开始检查第 {kkk + 1} 個機臺{unit_folder_path.name} ML 信息")
         pprint(parse_ml_folder(unit_folder_path, ml_types=ml_types))
         for ml_response_path in file_name(unit_folder_path, r'.*(SOBBK|ICEBK|BGI|SOB|ICE)\.txt'):
             ml_type = re.findall(
                 r'.*(SOBBK|ICEBK|BGI|SOB|ICE)\.txt', ml_response_path.name)[0]
             pic_path = ml_response_path.parent / \
-                (ml_response_path.stem + '.JPG')
+                       (ml_response_path.stem + '.JPG')
             if not pic_path.exists():
                 pic_path = file_name(unit_folder_path, fr'.*{ml_type}\.JPG')[0]
-            print(f"----第 {kk + 1} 個機臺{ml_type}信息檢查开始{pic_path.exists()}")
+            print(f"----第 {kkk + 1} 個機臺{ml_type}信息檢查开始{pic_path.exists()}")
             with open(ml_response_path, "r", encoding='utf-8') as f:
                 content = f.read()
             ml_response_status = re.findall(
@@ -90,8 +126,7 @@ def mark_bgi_ml_image():
             else:
                 ml_result = {}
             ml_pass_fail = ml_result.get('decision', status_code)
-            pic_new_name = f'{ml_type}_{"_".join(pic_path.stem.split('.')[:3])}_{
-                ml_pass_fail}'
+            pic_new_name = f'{ml_type}_{"_".join(pic_path.stem.split('.')[:3])}_{ml_pass_fail}'
             target_pic_path = str(
                 pic_path.parent / f'{pic_new_name}{pic_path.suffix}')
             print('------', f'目标图片路径{target_pic_path}')
@@ -118,9 +153,12 @@ def mark_bgi_ml_image():
                 cv2.putText(img, pic_new_name, (200, 100),
                             font, 3, (255, 0, 0), 5)
                 cv2.imwrite(target_pic_path, img)
-            print(f"----第 {kk + 1} 個機臺{ml_type}信息檢查完成")
-        print(f"--完成检查第 {kk + 1} 個機臺{unit_folder_path.name} ML 信息\n")
+            print(f"----第 {kkk + 1} 個機臺{ml_type}信息檢查完成")
+        print(f"--完成检查第 {kkk + 1} 個機臺{unit_folder_path.name} ML 信息\n")
 
 
 if __name__ == "__main__":
-    mark_bgi_ml_image()
+    for kk, test_folder_path in enumerate(data_pardir.iterdir()):
+        test_folder = UnitFolder(test_folder_path, station_name='CGS')
+        print(f'第{kk + 1}个{test_folder.station_name}UnitFolder[{test_folder.unit_folder_path}]读取成功..')
+        pprint(test_folder.parse_ml_folder())
